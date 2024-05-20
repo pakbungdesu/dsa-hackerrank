7b1f9092e7b9d2e70ce8426a7e0e2ce540850695
Pakbung Pimnara <142192211+pakbung2000@users.noreply.github.com>
2024-05-20 14:12:58 +0700

    Create solution.py

---
+++ Picking Numbers/solution.py
@@ -0,0 +1,14 @@
+
+def pickingNumbers(a):
+    diff = list(set(a))
+    a_list = [[a1, a2] for a1 in diff for a2 in diff if a2 - a1 == 1 or a1 == a2]
+    
+    max_ans = 0
+    for obj in a_list:
+        res = list(filter(lambda x: x in obj, a))
+        ans = len(res)
+        if ans > max_ans:
+            max_ans = ans
+            
+    return max_ans
+  
