b8c70bf6653790aced74907fdb2d02816c1686cf
Pimnara Sujaritchat <142192211+pakbungdesu@users.noreply.github.com>
2024-08-01 21:12:25 +0700

    Update twoChar_sol.py

--- Strings/Two Characters/twoChar_sol.py
+++ Strings/Two Characters/twoChar_sol.py
@@ -1,24 +1,23 @@
 import re
-import itertools
+from itertools import combinations
 
 def alternate(s):
-
+    
     letters = set(s)
-    num_letters = len(letters)
-    if num_letters == 1 and len(s) == 1:
+    n = len(letters)
+  
+    if n == 1:
         return 0
-    elif num_letters == 1:
-        return 0
-    elif len(s) == 2 and num_letters == 2:
-        return 2
     else:
-        maximum = 0
-        pattern = re.compile(r'^(\w)(?!\1)(\w)(\1\2)*\1?$')
-        for removals in itertools.combinations(letters, num_letters-2):
-            string = s
-            for letter in removals:
-                string = string.replace(letter, "")
-                if pattern.match(string):
-                    maximum = max(maximum, len(string))
-                    
-        return maximum
+        a = combinations(letters, n-2)
+        max_value = 0
+        
+        for ele1 in a:
+            x = s
+            for ele2 in ele1:
+                x = x.replace(ele2, "")
+            res = re.search(r"^(\w)(?!\1)(\w)(\1\2)*\1?$", x)
+            if res:
+                max_value = max(max_value, len(x))
+            
+        return max_value
