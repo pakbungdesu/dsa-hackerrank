7ff869f6762efd7fe0e00a4e642ef05422b711bc
Pimnara Sujaritchat <142192211+pakbungdesu@users.noreply.github.com>
2024-08-11 14:53:56 +0700

    Update stgr_count_sol.py

--- Implementation/Stranger Counter/stgr_count_sol.py
+++ Implementation/Stranger Counter/stgr_count_sol.py
@@ -1,13 +1,4 @@
 
-    # 3 + 6 = 9
-    # 6 * 2 = 12
-    
-    # 9 + 12 = 21
-    # 12 * 2 = 24
-    
-    # 21 + 24 = 45
-    # 24 * 2 = 48
-
 def strangeCounter(t):
 
     if t in range(1, 4):
@@ -18,7 +9,7 @@ def strangeCounter(t):
   
     for _ in range(t):
         new_x = x + y
-        y = y * 2
+        y *= 2
         if t in range(x+1, new_x+1):
             return new_x - t + 1
                   
