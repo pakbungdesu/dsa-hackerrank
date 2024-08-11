3ac49b3fc982f3565672bd84dcf9a06a081aa78c
Pimnara Sujaritchat <142192211+pakbungdesu@users.noreply.github.com>
2024-08-11 14:38:57 +0700

    Update restr_sol.py

--- Implementation/Repeated String/restr_sol.py
+++ Implementation/Repeated String/restr_sol.py
@@ -3,7 +3,7 @@ def repeatedString(s, n):
     multiply = n//len(s)
     reminder = n - (multiply*len(s))
     
-    reminder = s[0:reminder].count("a")
+    reminder = s[:reminder].count("a")
     res = s.count("a") * multiply
     
     return res + reminder
